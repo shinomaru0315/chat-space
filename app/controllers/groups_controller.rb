ac6ea22f04df9172a52ae4d614b48f9f2d26da25
shinomaru0315 <frb_shinomaru@yahoo.co.jp>
2019-12-13 15:25:38 +0900

    side_ber　機能実装完了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group,only: [:edit, :update]
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -18,11 +20,19 @@ class GroupsController < ApplicationController
   end
 
   def update
-
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [] )
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
