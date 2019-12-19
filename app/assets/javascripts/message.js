35a339f94de7298241ab8aaf1dc3984bf52a32f2
shinomaru0315 <frb_shinomaru@yahoo.co.jp>
2019-12-19 20:35:33 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -14,7 +14,7 @@ $(function(){
           <p class="lower-message__content">
             ${message.content}
           </p>
-          <img src= "${message.image.url}" " class="lower-message__image">
+          <img src= "${message.image}" " class="lower-message__image">
         </div>
       </div>`
     } else if (message.content) {
@@ -44,43 +44,36 @@ $(function(){
             ${message.date}
           </div>
         <div class="lower-message">
-          <img src= "${message.image.url}" " class="lower-message__image">
+          <img src= "${message.image}" " class="lower-message__image">
         </div>
       </div>`
     };
-
     return html;
   };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(message) {
-        console.log(message)
-        //追加するHTMLの入れ物を作る
-        var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        insertHTML = buildHTML(message)
-        // $.each(messages, function(i, message) {
-        //   console.log(message)
-        // });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.messages').append(insertHTML);
-      })
-      .fail(function(){
-        alert('メッセージ送信に失敗しました');
-      });
-      $('submit').prop('disabled', true);
-      return false;
-  });
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+     .done(function(data){
+       var html = buildHTML(data);
+       $('.main__chat').append(html);
+       $('.main__chat').animate({ scrollTop: $('.main__chat')[0].scrollHeight});
+       $('form')[0].reset();
+     })
+     .fail(function(){
+      alert('メッセージ送信に失敗しました');
+    });
+    $('submit').prop('disabled', true);
+    return false;
+  })
   var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     last_message_id = $('.message:last').data("message-id");
